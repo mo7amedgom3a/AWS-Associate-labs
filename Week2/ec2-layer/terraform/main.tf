b4f00eedc3041b8a7aec8a899463335db66d0cf0
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2025-07-30 19:03:50 +0300

    Update security group to allow SSH access from all IPs and remove dynamic IP check

--- Week2/ec2-layer/terraform/main.tf
+++ Week2/ec2-layer/terraform/main.tf
@@ -2,9 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "http" "my_ip" {
-  url = "https://checkip.amazonaws.com"
-}
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
@@ -78,7 +75,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
